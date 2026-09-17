7561ebcb4b67d0ea63b122269b9c653561fb8a50
imbertjules <imbertjules@gmail.com>
2026-09-17 12:45:25 +0200

    globals.css

--- src/app/projets/[slug]/page.tsx
+++ src/app/projets/[slug]/page.tsx
@@ -24,19 +24,32 @@ export default async function ProjectPage({
   return (
     <main className="project-page">
       <SiteHeader settings={site.settings} />
-      <div className="project-gallery">
-        {project.images.map((image) => (
-          <figure key={image.src} className="project-figure">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src={image.src} alt={project.title} />
-            {image.caption ? (
-              <figcaption
-                className="project-caption"
-                dangerouslySetInnerHTML={{ __html: image.caption }}
+      <div className="grid grid-cols-12 gap-y-32 gap-x-8 max-w-[1600px] mx-auto px-8 py-20">
+        {project.images.map((image, index) => {
+          const pattern = index % 4;
+          const layoutClasses =
+            pattern === 0 ? "col-span-7 col-start-1" :
+            pattern === 1 ? "col-span-6 col-start-7" :
+            pattern === 2 ? "col-span-4 col-start-4" :
+            "col-span-8 col-start-3";
+
+          return (
+            <figure key={image.src} className={`flex flex-col ${layoutClasses}`}>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src={image.src}
+                alt={project.title}
+                className="w-full h-auto block object-cover"
               />
-            ) : null}
-          </figure>
-        ))}
+              {image.caption ? (
+                <figcaption
+                  className="block font-serif font-bold text-[16px] leading-tight mt-5 max-w-xl text-white"
+                  dangerouslySetInnerHTML={{ __html: image.caption }}
+                />
+              ) : null}
+            </figure>
+          );
+        })}
       </div>
     </main>
   );
